b9bcb2ae384729d877d6a6cd7f16b77559e35bd7
123 <chenjiuxiang1994@gmail.com>
2020-08-25 23:54:02 -0400

    first

---
+++ StudentManagerPro/FrmScoreQuery.designer.cs
@@ -0,0 +1,271 @@
+﻿namespace StudentManager
+{
+    partial class FrmScoreQuery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmScoreQuery));
+            this.dgvScoreList = new System.Windows.Forms.DataGridView();
+            this.StudentId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StudentName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ClassName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CSharp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SQLServerDB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.cboClass = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnGetCourse = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvScoreList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvScoreList
+            // 
+            this.dgvScoreList.AllowUserToAddRows = false;
+            this.dgvScoreList.AllowUserToDeleteRows = false;
+            this.dgvScoreList.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvScoreList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.StudentId,
+            this.StudentName,
+            this.ClassName,
+            this.CSharp,
+            this.SQLServerDB});
+            this.dgvScoreList.Location = new System.Drawing.Point(13, 111);
+            this.dgvScoreList.Name = "dgvScoreList";
+            this.dgvScoreList.ReadOnly = true;
+            this.dgvScoreList.RowTemplate.Height = 23;
+            this.dgvScoreList.Size = new System.Drawing.Size(759, 326);
+            this.dgvScoreList.TabIndex = 8;
+            // 
+            // StudentId
+            // 
+            this.StudentId.DataPropertyName = "StudentId";
+            this.StudentId.HeaderText = "学号";
+            this.StudentId.Name = "StudentId";
+            this.StudentId.ReadOnly = true;
+            // 
+            // StudentName
+            // 
+            this.StudentName.DataPropertyName = "StudentName";
+            this.StudentName.HeaderText = "姓名";
+            this.StudentName.Name = "StudentName";
+            this.StudentName.ReadOnly = true;
+            this.StudentName.Width = 120;
+            // 
+            // ClassName
+            // 
+            this.ClassName.DataPropertyName = "ClassName";
+            this.ClassName.HeaderText = "班级";
+            this.ClassName.Name = "ClassName";
+            this.ClassName.ReadOnly = true;
+            this.ClassName.Width = 120;
+            // 
+            // CSharp
+            // 
+            this.CSharp.DataPropertyName = "CSharp";
+            this.CSharp.HeaderText = "C#成绩";
+            this.CSharp.Name = "CSharp";
+            this.CSharp.ReadOnly = true;
+            this.CSharp.Width = 80;
+            // 
+            // SQLServerDB
+            // 
+            this.SQLServerDB.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.SQLServerDB.DataPropertyName = "SQLServerDB";
+            this.SQLServerDB.HeaderText = "数据库成绩";
+            this.SQLServerDB.Name = "SQLServerDB";
+            this.SQLServerDB.ReadOnly = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Image = ((System.Drawing.Image)(resources.GetObject("btnClose.Image")));
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(678, 63);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(85, 33);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "关闭窗口";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // cboClass
+            // 
+            this.cboClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboClass.FormattingEnabled = true;
+            this.cboClass.Location = new System.Drawing.Point(82, 73);
+            this.cboClass.Name = "cboClass";
+            this.cboClass.Size = new System.Drawing.Size(121, 20);
+            this.cboClass.TabIndex = 10;
+            this.cboClass.SelectedIndexChanged += new System.EventHandler(this.cboClass_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "学员班级：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(220, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "按C#成绩快速浏览： C# >";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(372, 73);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(38, 21);
+            this.txtScore.TabIndex = 12;
+            this.txtScore.TextChanged += new System.EventHandler(this.txtScore_TextChanged);
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.Location = new System.Drawing.Point(541, 63);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(131, 33);
+            this.btnShowAll.TabIndex = 14;
+            this.btnShowAll.Text = "显示全部成绩";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(416, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "分";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label9.ForeColor = System.Drawing.Color.Purple;
+            this.label9.Location = new System.Drawing.Point(12, 24);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(255, 33);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "学员成绩快速浏览";
+            // 
+            // btnGetCourse
+            // 
+            this.btnGetCourse.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnGetCourse.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btnGetCourse.Location = new System.Drawing.Point(605, 453);
+            this.btnGetCourse.Name = "btnGetCourse";
+            this.btnGetCourse.Size = new System.Drawing.Size(168, 40);
+            this.btnGetCourse.TabIndex = 20;
+            this.btnGetCourse.Text = "点击查看完整.NET课程";
+            this.btnGetCourse.UseVisualStyleBackColor = false;
+            this.btnGetCourse.Click += new System.EventHandler(this.btnGetCourse_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(14, 461);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(247, 20);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "请学员朋友认真按照视频完成本功能！";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label10.ForeColor = System.Drawing.Color.Black;
+            this.label10.Location = new System.Drawing.Point(262, 461);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(311, 20);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "VIP系统学习咨询QQ：995551857  9995551855";
+            // 
+            // FrmScoreQuery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 510);
+            this.Controls.Add(this.btnGetCourse);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnShowAll);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.cboClass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.dgvScoreList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FrmScoreQuery";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "考试成绩快速浏览";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvScoreList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvScoreList;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ComboBox cboClass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.Button btnShowAll;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ClassName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CSharp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SQLServerDB;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnGetCourse;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label10;
+    }
+}
